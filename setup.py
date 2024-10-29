2c2fa23d423b8b073eda64474cccd036052da72d
Muhammad Umair Danish <112751117+udanish50@users.noreply.github.com>
2024-10-29 16:20:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="midipy",
-    version="0.1.1",
+    version="0.1.2",
     author="Your Name",
     author_email="your.email@example.com",
     description="A Python package for MIDI data processing, analysis, and parsing.",
